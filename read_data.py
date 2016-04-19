30a7b1685b8f0db55209d8d3a983dfd9012bb933
Dirk Hesse <dirk.hesse@intelcom.no>
2016-04-19 18:52:51 +0200

    Adding clean_data method.

--- read_data.py
+++ read_data.py
@@ -5,6 +5,10 @@ def extract_year(df):
     df['year'] = df.Date.apply(lambda x: int(x[4:8]))
     return df
 
+def clean_data(df):
+    return df[(df.item == 'Sisal') &
+              (df.Unit == 'Standard Local Currency/tonne')]
+
 class TestExtractYear(unittest.TestCase):
     def test_known_values(self):
         test_data = pd.DataFrame({'Date': ["1-1-1234"]})
@@ -13,5 +17,18 @@ class TestExtractYear(unittest.TestCase):
             extract_year(test_data).year,
             expected_result)
 
+class TestCleanData(unittest.TestCase):
+    def setUp(self):
+        self.slc = 'Standard Local Currency/tonne'
+        self.test_data = pd.DataFrame(
+            {'item': ["Sisal", "Hay"]*2,
+             'Unit': [self.slc, self.slc, 'dollar', 'dollar']})
+    def test_cleans_non_sisal(self):
+        result = clean_data(self.test_data)
+        self.assertTrue(all(result.item == "Sisal"))
+    def test_cleans_non_slc(self):
+        result = clean_data(self.test_data)
+        self.assertTrue(all(result.Unit == self.slc))
+        
 if __name__ == "__main__":
     unittest.main()
