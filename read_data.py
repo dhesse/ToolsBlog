44edf49e57409b6092d693480f149c9ce1ecce07
Dirk Hesse <dirk.hesse@intelcom.no>
2016-04-19 19:12:38 +0200

    Adding more exception tests.

--- read_data.py
+++ read_data.py
@@ -1,30 +1,36 @@
 import unittest
 import pandas as pd
 
-class NoDateInData(Exception):
+class NoDateInDataError(Exception):
     pass
 
-class NoSisalInData(Exception):
+class NoSisalInDataError(Exception):
     pass
 
-class NoStandardCurrencyInData(Exception):
+class NoStandardCurrencyInDataError(Exception):
     pass
 
 def extract_year(df):
     if 'Date' not in df:
-        raise NoDateInData()
+        raise NoDateInDataError()
     df['year'] = df.Date.apply(lambda x: int(x[4:8]))
     return df
 
+SLU = 'Standard Local Currency/tonne'
+
 def clean_data(df):
+    if not any(df.item == 'Sisal'):
+        raise NoSisalInDataError
+    if not any(df.Unit == SLU):
+        raise NoStandardCurrencyInDataError
     return df[(df.item == 'Sisal') &
-              (df.Unit == 'Standard Local Currency/tonne')]
+              (df.Unit == SLU)]
 
 class TestExtractYear(unittest.TestCase):
     """You should test more here, connected to the expected string
     format in the date column."""
     def test_no_Date_column_raises(self):
-        with self.assertRaises(NoDateInData):
+        with self.assertRaises(NoDateInDataError):
             extract_year(pd.DataFrame({}))
     def test_known_values(self):
         test_data = pd.DataFrame({'Date': ["1-1-1234"]})
@@ -35,18 +41,26 @@ class TestExtractYear(unittest.TestCase):
 
 class TestCleanData(unittest.TestCase):
     def setUp(self):
-        self.slc = 'Standard Local Currency/tonne'
         self.test_data = pd.DataFrame(
             {'item': ["Sisal", "Hay"]*2,
-             'Unit': [self.slc, self.slc, 'dollar', 'dollar']})
+             'Unit': [SLU, SLU, 'dollar', 'dollar']})
     def test_cleans_non_sisal(self):
         result = clean_data(self.test_data)
         self.assertTrue(all(result.item == "Sisal"))
     def test_cleans_non_slc(self):
         result = clean_data(self.test_data)
-        self.assertTrue(all(result.Unit == self.slc))
+        self.assertTrue(all(result.Unit == SLU))
     def test_no_sisal_raises(self):
-        pass
+        with self.assertRaises(NoSisalInDataError):
+            clean_data(pd.DataFrame(
+                {'item': [None],
+                 'Unit': [SLU]}))
+    def test_no_slc_raises(self):
+        with self.assertRaises(NoStandardCurrencyInDataError):
+            clean_data(pd.DataFrame(
+                {'item': ['Sisal'],
+                 'Unit': [None]}))
+            
         
 if __name__ == "__main__":
     unittest.main()
